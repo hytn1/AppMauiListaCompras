044824653f7b7e9b5313e0fee91e6ac60645e939
Usuario <Usuario@CPD02-M07>
2024-03-07 15:02:56 -0300

    Adicionar arquivos de projeto.

---
+++ AppMauiListaCompras/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AppMauiListaCompras.Helpers;
+
+namespace AppMauiListaCompras
+
+{
+    public partial class App : Application
+    {
+        static SQliteDatabaseHelper _db;
+       
+        public static SQliteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData
+                        ),"banco_sqlite_compras.db3"
+                        );
+
+                    _db = new SQliteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
+        
+    }
+
+}
